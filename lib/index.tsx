c4283d844162cc8e5fe56b22e98c9b8feacfe6d1
LBJ <576117134@qq.com>
2019-08-21 18:12:45 +0800

    把webpack.config分开

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
 
+const a:number = 1
+
 ReactDOM.render(<Button/>,document.body)
 
 console.log(React)
 console.log(ReactDOM)
+
+export {Button,a}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: 'development',
+    // mode: 'development',
     entry: {
         index: './lib/index.tsx'
     },
@@ -21,24 +21,24 @@ module.exports = {
             }
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'MYUI',
-            template: 'index.html'
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React',
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM',
-        }
-    }
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: 'MYUI',
+    //         template: 'index.html'
+    //     })
+    // ],
+    // externals: {
+    //     react: {
+    //         commonjs: 'react',
+    //         commonjs2: 'react',
+    //         amd: 'react',
+    //         root: 'React',
+    //     },
+    //     'react-dom': {
+    //         commonjs: 'react-dom',
+    //         commonjs2: 'react-dom',
+    //         amd: 'react-dom',
+    //         root: 'ReactDOM',
+    //     }
+    // }
 }
