7e7a4013c77a3732df837dcefce4f655da63437c
LBJ <576117134@qq.com>
2019-08-26 18:31:37 +0800

    upload 0.0.2 version

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
-
-const fn:React.MouseEventHandler = (e) => {
-  console.log(e.target)
-}
-
-ReactDOM.render(
-    <div>
-      {/* <Icon name="wechat" className="sss" onClick={fn}/>
-      <Icon name="qq" onMouseEnter={fn}/> */}
-      <Icon name="alipay" onMouseEnter={fn} onMouseLeave={fn}/>  
-    </div>
-,
-document.querySelector('#root'))
+export {default as Icon} from './icon/icon'
