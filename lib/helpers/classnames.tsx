4382d321154a80f7b8ab39c56830fb5e4f75b5e4
LBJ <576117134@qq.com>
2019-09-06 17:08:16 +0800

    函数式 getFirstClassName

--- lib/helpers/classnames.tsx
+++ lib/helpers/classnames.tsx
@@ -19,23 +19,16 @@ interface ClassToggle {
 }
 
 function getFirstClassName(perfix: string) {
-    return function (name: string | ClassToggle, options?: Options) {
-
-        const name2 = (typeof name === 'string' || typeof name === 'undefined') ?
-            { [name]: name } : name
-        const scope = Object.entries(name2)
+    return (name: string | ClassToggle, options?: Options) => 
+        Object
+            .entries(name instanceof Object ? name : { [name]: name })
             .filter(kv => kv[1] !== false)
             .map(kv => kv[0])
-            .map(name =>
-                [perfix, name].filter(Boolean).join('-')
-            ).join(' ')
-
-        if (options && options.extra) {
-            return [scope, options.extra].filter(Boolean).join(' ')
-        } else {
-            return scope
-        }
-    }
+            .map(name => [perfix, name]
+                .filter(Boolean)
+                .join('-'))
+            .concat(options && options.extra || [])
+            .join(' ')  
 }
 
 export { getFirstClassName }
