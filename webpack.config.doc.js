2d21c1b7a4a718c6da52ec0faaa4a3c5c992d1bf
LBJ <576117134@qq.com>
2019-09-11 16:14:23 +0800

    fix webpack.config.doc.js

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -13,7 +13,7 @@ module.exports = Object.assign({},base,{
         new HtmlWebpackPlugin({
             title: 'MYUI',
             template: 'example.html',
-            filename:'example.html'
+            filename:'index.html'
         })
     ],
 }) 
