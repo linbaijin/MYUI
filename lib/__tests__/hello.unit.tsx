ed14ea03e2febfb137748a506bbad3ea185d12b1
LBJ <576117134@qq.com>
2019-09-19 11:34:01 +0800

    update

--- lib/__tests__/hello.unit.tsx
+++ lib/__tests__/hello.unit.tsx
@@ -1,6 +1,6 @@
 import render from 'react-test-renderer'
 import React from 'react'
-import Button from '../button'
+import Button from '../button/button'
 
 // function add (a:number,b:number):number{
 //     return a + b
