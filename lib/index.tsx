82fc3484cbc2b66ba0c502f4c8da513b0cd25d24
LBJ <576117134@qq.com>
2019-09-19 11:29:28 +0800

    update index.tsx

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,6 @@
 import './index.scss'
 
-export {default as Icon} from './icon/icon'
+export {default as Icon} from './icon/icon';
+export {default as Dialog} from './dialog/dialog';
+export {default as layout} from './layout/layout';
+
