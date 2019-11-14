417e379459a5ff6e92bc1b4db88370477c6c3237
LeeBriken <576117134@qq.com>
2019-11-14 22:25:39 +0800

    为scrollbar添加动画

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -7,54 +7,65 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 }
 
-const Scroll:React.FunctionComponent<Props> = (props) => {
-    const {children, ...rest} = props
+// const isTouchDevice: boolean = 'ontouchstart' in document.documentElement
+
+const Scroll: React.FunctionComponent<Props> = (props) => {
+    const { children, ...rest } = props
     const containerRef = useRef<HTMLDivElement>(null)
     const [barHeight, setBarHeight] = useState(0)
+    const [barVisible, setBarVisible] = useState(false)
     const [barTop, _setBarTop] = useState(0)
-    const setBarTop = (number:number) => {
-        if(number<0) {return}
-        const {current} = containerRef
+    const timeIdRef = useRef<number | null>(null)
+    const setBarTop = (number: number) => {
+        if (number < 0) { return }
+        const { current } = containerRef
         const scrollHeight = current!.scrollHeight
         const viewHeight = current!.getBoundingClientRect().height
         const maxBarTop = (scrollHeight - viewHeight) * viewHeight / scrollHeight
-        if(number > maxBarTop){return}
+        if (number > maxBarTop) { return }
         _setBarTop(number)
     }
-    const onScroll:UIEventHandler<HTMLDivElement> = () => {
-        const {current} = containerRef
+    const onScroll: UIEventHandler<HTMLDivElement> = () => {
+        setBarVisible(true)
+        const { current } = containerRef
         const scrollHeight = current!.scrollHeight
         const viewHeight = current!.getBoundingClientRect().height
         const scrollTop = current!.scrollTop
-        setBarTop(scrollTop * viewHeight / scrollHeight) 
+        setBarTop(scrollTop * viewHeight / scrollHeight)
+        if(timeIdRef !== null) {
+            window.clearTimeout(timeIdRef.current!)
+        }
+        timeIdRef.current = window.setTimeout(() => {
+            setBarVisible(false)
+        },1000)
     }
     useEffect(() => {
         const scrollHeight = containerRef.current!.scrollHeight//总滚动高度
         const viewHeight = containerRef.current!.getBoundingClientRect().height//可视范围高度
         setBarHeight(viewHeight * viewHeight / scrollHeight)
-    },[])
+    }, [])
 
     useEffect(() => {
-        document.addEventListener('mousemove',onMouseMoveBar)
-        document.addEventListener('mouseup',onMouseUpBar)
-        document.addEventListener('selectstart',onSelect)
+        document.addEventListener('mousemove', onMouseMoveBar)
+        document.addEventListener('mouseup', onMouseUpBar)
+        document.addEventListener('selectstart', onSelect)
         return () => {
-            document.removeEventListener('mousemove',onMouseMoveBar)
-            document.removeEventListener('mouseup',onMouseUpBar)
+            document.removeEventListener('mousemove', onMouseMoveBar)
+            document.removeEventListener('mouseup', onMouseUpBar)
         }
-    },[])
+    }, [])
 
     const draggingRef = useRef(false)
     const firstYRef = useRef(0)
     const firstBarTopRef = useRef(0)
-    const onMouseDownBar:MouseEventHandler = (e) => {
+    const onMouseDownBar: MouseEventHandler = (e) => {
         draggingRef.current = true
         firstYRef.current = e.clientY //记录mouseDown的初始位置
         firstBarTopRef.current = barTop //记录bar的初始位置
         console.log('start')
     }
-    const onMouseMoveBar = (e:MouseEvent) => {
-        if(draggingRef.current){
+    const onMouseMoveBar = (e: MouseEvent) => {
+        if (draggingRef.current) {
             const delta = e.clientY - firstYRef.current //鼠标移动的距离
             const newBarTop = firstBarTopRef.current + delta
             setBarTop(newBarTop)
@@ -63,26 +74,29 @@ const Scroll:React.FunctionComponent<Props> = (props) => {
             containerRef.current!.scrollTop = newBarTop * scrollHeight / viewHeight
         }
     }
-    const onMouseUpBar = (e:MouseEvent) => {
+    const onMouseUpBar = (e: MouseEvent) => {
         draggingRef.current = false
         console.log('end')
     }
 
-    const onSelect = (e:Event) => {
+    const onSelect = (e: Event) => {
         if (draggingRef.current) {
             e.preventDefault()
         }
     }
 
     return <div className="myui-scroll" {...rest}>
-        <div className="myui-scroll-inner" onScroll={onScroll} style={{right:-ScrollbarWidth()}}
+        <div className="myui-scroll-inner" onScroll={onScroll} style={{ right: -ScrollbarWidth() }}
             ref={containerRef}
-        >   
+        >
             {children}
         </div>
-        <div className="myui-scroll-track" onMouseDown={onMouseDownBar}>
-            <div className="myui-scroll-bar" style={{height:barHeight,transform:`translateY(${barTop}px)`}}></div>
-        </div>
+        {
+            <div className="myui-scroll-track" onMouseDown={onMouseDownBar}>
+                <div className="myui-scroll-bar" style={{ height: barHeight, transform: `translateY(${barTop}px)`,opacity: barVisible ? 1 : 0}}></div>
+            </div>
+        }
+
     </div>
 }
 
