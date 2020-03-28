6686e10c967ca80a09ff704c823e5ac9b8917efe
LeeBriken <576117134@qq.com>
2020-03-28 12:15:01 +0800

    add useUpdate.tsx

--- lib/tree/tree-item.tsx
+++ lib/tree/tree-item.tsx
@@ -1,24 +1,25 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {getFirstClassName} from "../helpers/classnames";
 import Icon from "../icon/icon";
+import useUpdate from "../hooks/useUpdate";
 
 
-const TreeItem:React.FC<RenderTreeItem> = (props) => {
-    const {level,treeProps,treeItem} = props
+const TreeItem: React.FC<RenderTreeItem> = (props) => {
+    const {level, treeProps, treeItem} = props
     const sc = getFirstClassName('myui-tree')
-    const onChangeNoChildren = (treeItem:TreeDataItem,isSelected:boolean)=> {
-        if(treeProps.multiple) {
-            isSelected?treeProps.onChange([...treeProps.selected,treeItem.title]):
-                treeProps.onChange(treeProps.selected.filter(v => v!==treeItem.title))
+    const onChangeNoChildren = (treeItem: TreeDataItem, isSelected: boolean) => {
+        if (treeProps.multiple) {
+            isSelected ? treeProps.onChange([...treeProps.selected, treeItem.title]) :
+                treeProps.onChange(treeProps.selected.filter(v => v !== treeItem.title))
         }
 
     }
-    let allTitle:string[] = []
-    const findAllTitle = (treeItem:TreeDataItem) => {
-        if(treeItem.children) {
-            for (let i = 0;i<treeItem.children.length;i++) {
+    let allTitle: string[] = []
+    const findAllTitle = (treeItem: TreeDataItem) => {
+        if (treeItem.children) {
+            for (let i = 0; i < treeItem.children.length; i++) {
                 allTitle.push(treeItem.children[i].title)
-                if(treeItem.children[i].children) {
+                if (treeItem.children[i].children) {
                     findAllTitle(treeItem.children[i])
                 }
             }
@@ -28,53 +29,81 @@ const TreeItem:React.FC<RenderTreeItem> = (props) => {
         }
     }
 
-    const onChangeHasChildren = (treeItem:TreeDataItem,isSelected:boolean) => {
-        if(treeProps.multiple&&treeItem.children) {
-            isSelected?treeProps.onChange([...treeProps.selected,...findAllTitle(treeItem),treeItem.title]):
+    const onChangeHasChildren = (treeItem: TreeDataItem, isSelected: boolean) => {
+        if (treeProps.multiple && treeItem.children) {
+            isSelected ? treeProps.onChange([treeItem.title, ...treeProps.selected, ...findAllTitle(treeItem)]) :
                 treeProps.onChange(treeProps.selected.filter(v => {
-                    return !([...findAllTitle(treeItem),treeItem.title].includes(v))
+                    return !([...findAllTitle(treeItem), treeItem.title].includes(v))
                 }))
         }
     }
-    const isAllSelected = (treeItem:TreeDataItem) => {
-        return treeItem.children?.map((item)=>treeProps.selected.includes(item.title)).every(Boolean)
+    const isAllSelected = (treeItem: TreeDataItem) => {
+        return treeItem.children?.map((item) => treeProps.selected.includes(item.title)).every(Boolean)
     }
 
-    useEffect(()=>{
-        if(!isAllSelected(treeItem)){
-            if(treeProps.selected.includes(treeItem.title)) {
-                treeProps.multiple&&treeProps.onChange(treeProps.selected.filter(v => v !== treeItem.title))
+    useEffect(() => {
+        if (!isAllSelected(treeItem)) {
+            if (treeProps.selected.includes(treeItem.title)) {
+                treeProps.multiple && treeProps.onChange(treeProps.selected.filter(v => v !== treeItem.title))
             }
-        }else {
-            if(!treeProps.selected.includes(treeItem.title)) {
-                treeProps.multiple&&treeProps.onChange([...treeProps.selected,treeItem.title])
+        } else {
+            if (!treeProps.selected.includes(treeItem.title)) {
+                treeProps.multiple && treeProps.onChange([...treeProps.selected, treeItem.title])
             }
         }
-    },[isAllSelected(treeItem)])
+    }, [isAllSelected(treeItem)])
+
+    const expand = () => {
+        setExpanded(true)
+    }
+
+    const collapse = () => {
+        setExpanded(false)
+    }
+
+    const [expanded, setExpanded] = useState(true)
+
+
+    useUpdate(expanded, () => {
+        console.log('expanded变为', expanded)
+    })
 
     return (
         <div>
             {
                 <div key={treeItem.key}
-                     className={sc({[`level-${level}`]:true,'item':true})}
+                     className={sc({[`level-${level}`]: true, 'item': true})}
                 >
                     <div className={sc('text')}>
                         <Icon name="floder_close"/>
                         <input
                             type="checkbox"
-                            onChange={(e) => treeItem.children?
-                                onChangeHasChildren(treeItem,e.target.checked):
-                                onChangeNoChildren(treeItem,e.target.checked)}
-                            checked={treeItem.children&&isAllSelected(treeItem)||treeProps.selected.indexOf(treeItem.title)>=0}
+                            onChange={(e) => treeItem.children ?
+                                onChangeHasChildren(treeItem, e.target.checked) :
+                                onChangeNoChildren(treeItem, e.target.checked)}
+                            checked={treeItem.children && isAllSelected(treeItem) || treeProps.selected.indexOf(treeItem.title) >= 0}
                         />
-                        {treeItem.title}</div>
-                    {treeItem.children?.map(item=>
-                        <TreeItem
-                            key={item.title}
-                            treeItem={item}
-                            treeProps={treeProps}
-                            level={level+1}
-                        />)}
+                        {treeItem.title}
+                        {
+                            treeItem.children &&
+                            <span onSelect={e => e.preventDefault()}>
+                                    {
+                                        expanded ?
+                                            <span onClick={collapse}>-</span> :
+                                            <span onClick={expand}>+</span>
+                                    }
+                                </span>
+                        }
+                    </div>
+                    <div className={sc({children: true, collapsed: !expanded})}>
+                        {treeItem.children?.map(item =>
+                            <TreeItem
+                                key={item.title}
+                                treeItem={item}
+                                treeProps={treeProps}
+                                level={level + 1}
+                            />)}
+                    </div>
                 </div>
             }
 
