8230568a707ea061b6b1ee8727287fbc62b434bc
LeeBriken <576117134@qq.com>
2020-03-29 14:36:16 +0800

    clone ChildNode && appedNode

--- lib/slider/slider.example.tsx
+++ lib/slider/slider.example.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Slider from "./slider";
+import './slider.demo.scss'
 
 const SliderExample = () => {
-    return <div>
-        <Slider/>
+    return <div style={{width: 500, border: '1px solid red'}}>
+        <Slider>
+            <div className="slider-demo">1</div>
+            <div className="slider-demo">2</div>
+            <div className="slider-demo">3</div>
+        </Slider>
     </div>
 }
 
--- lib/slider/slider.tsx
+++ lib/slider/slider.tsx
@@ -1,10 +1,81 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './slider.scss'
+import {getFirstClassName} from "../helpers/classnames";
 
-const Slider = () => {
-    return <div>
-        Slider
-    </div>
+interface SliderProps {
+    duration?: number
+    dots?: boolean
+    beforeChaneg?: (from: number, to: number) => void
+    afterChange?: (from: number, to: number) => void
+}
+
+const handleMouseEnter = () => {
+
+}
+
+const handleMouseLeave = () => {
+
+}
+
+const Slider: React.FC<SliderProps> = (props) => {
+    const {children, dots} = props
+    const sc = getFirstClassName('myui-slider')
+    const containerRef = useRef<HTMLDivElement>(null)
+    const lengthRef = useRef(0)
+    const [current, setCurrent] = useState<number>(1)
+
+    const cloneNode = () => {
+        const nodeList: HTMLElement[] = []
+        containerRef.current?.childNodes.forEach(node => {
+            if (node.nodeType === 1) {
+                const eleNode = node as HTMLElement
+                nodeList.push(eleNode)
+                eleNode.style.flexShrink = '0'
+            }
+        })
+        lengthRef.current = nodeList.length
+        console.log(nodeList);
+        containerRef.current?.append(nodeList[0].cloneNode(true))
+        containerRef.current?.prepend(nodeList[nodeList.length - 1].cloneNode(true))
+    }
+
+    useEffect(() => {
+        cloneNode()
+    }, [])
+
+    return (
+        <div
+            className={sc('')}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div
+                className={sc('container')}
+                ref={containerRef}
+            >
+                {children}
+            </div>
+            {
+                dots && (
+                    <div className={sc('dots-wrapper')}>
+                        {
+                            React.Children.map(children, (child, index) => (
+                                <span
+                                    className={sc({dot: true, 'dot-active': current === index + 1})}
+                                    onClick={() => setCurrent(index + 1)}
+                                />
+                            ))
+                        }
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+Slider.defaultProps = {
+    duration: 0,
+    dots: true
 }
 
 export default Slider
